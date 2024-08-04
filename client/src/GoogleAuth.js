03ea5017fcc3309b0dce7bdd46b21f6138d2face
muzzkit <muzkit48@gmail.com>
2024-08-04 14:01:59 +0100

    update clientId

--- client/src/GoogleAuth.js
+++ client/src/GoogleAuth.js
@@ -7,7 +7,7 @@ class GoogleAuth extends React.Component {
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
-                clientId: '',
+                clientId: 'INSERT YOUR CLIENTID',
                 scope: 'email',
                 plugin_name: 'Streams'
             }).then(() => {
